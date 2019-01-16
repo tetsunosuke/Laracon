782af911b293fa4c222098eff83ac6e7a85b18e9
FORTE <fortegp05@hotmail.com>
2019-01-16 10:27:18 +0900

    テスト漏れ修正

--- question/q001/q001.php
+++ question/q001/q001.php
@@ -6,7 +6,7 @@
 
 use LaravelJpCon\Q001\Wareki;
 
-require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 if ($argc < 2) return Wareki::ERROR_MSG;
 
