aaf6c9a99c51be57445f5896cef2fa58acf40090
FORTE <fortegp05@hotmail.com>
2019-01-22 23:34:01 +0900

    add q003

---
+++ tests/BlackjackTest.php
@@ -0,0 +1,85 @@
+<?php
+
+use LaravelJpCon\q003\Blackjack;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Created by PhpStorm.
+ * User: fortegp05
+ */
+class BlackjackTest extends TestCase
+{
+    private $blackjack;
+    private $methodCardOpen;
+    private $propertyMyPoint;
+    private $propertyDealerPoint;
+
+    /**
+     * 初期設定
+     */
+    public function setUp()
+    {
+        $this->blackjack = new Blackjack();
+        $blackjackReflection = new \ReflectionClass($this->blackjack);
+
+        $this->methodCardOpen = $blackjackReflection->getMethod('cardOpen');
+        $this->methodCardOpen->setAccessible(true);
+
+        $this->propertyMyPoint = $blackjackReflection->getProperty('myPoint');
+        $this->propertyMyPoint->setAccessible(true);
+
+        $this->propertyDealerPoint = $blackjackReflection->getProperty('dealerPoint');
+        $this->propertyDealerPoint->setAccessible(true);
+    }
+
+    /**
+     * 引き分けテスト
+     */
+    public function test_cardOpenDraw()
+    {
+        $playerPoint = 21;
+        $dealerPoint = 21;
+        $this->propertyMyPoint->setValue($this->blackjack, $playerPoint);
+        $this->propertyDealerPoint->setValue($this->blackjack, $dealerPoint);
+
+        ob_start();
+        $this->methodCardOpen->invoke($this->blackjack);
+        $actual = ob_get_clean();
+
+        $this->assertSame('あなたは' . $playerPoint . '点、ディーラーは' . $dealerPoint . '点で、引き分けです!' . PHP_EOL, $actual, '引き分け');
+    }
+
+    /**
+     * プレイヤー勝利テスト
+     */
+    public function test_cardOpenPlayerWin()
+    {
+        $playerPoint = 21;
+        $dealerPoint = 10;
+        $this->propertyMyPoint->setValue($this->blackjack, $playerPoint);
+        $this->propertyDealerPoint->setValue($this->blackjack, $dealerPoint);
+
+        ob_start();
+        $this->methodCardOpen->invoke($this->blackjack);
+        $actual = ob_get_clean();
+
+        $this->assertSame('あなたは' . $playerPoint . '点、ディーラーは' . $dealerPoint . '点で、あなたの勝ちです!' . PHP_EOL, $actual, '引き分け');
+    }
+
+    /**
+     * ディーラー勝利テスト
+     */
+    public function test_cardOpenDealerWin()
+    {
+        $playerPoint = 10;
+        $dealerPoint = 21;
+        $this->propertyMyPoint->setValue($this->blackjack, $playerPoint);
+        $this->propertyDealerPoint->setValue($this->blackjack, $dealerPoint);
+
+        ob_start();
+        $this->methodCardOpen->invoke($this->blackjack);
+        $actual = ob_get_clean();
+
+        $this->assertSame('あなたは' . $playerPoint . '点、ディーラーは' . $dealerPoint . '点で、ディーラーの勝ちです!' . PHP_EOL, $actual, '引き分け');
+    }
+}
